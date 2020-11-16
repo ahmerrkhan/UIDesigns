6273ed6d93e630730018d2be64fd2b6df66e7e2b
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-11-16 18:46:32 +0500

    two numbers addition
    adding two digits and their output

---
+++ addtwonumbers/lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main()  => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final _apptitle = "ADD TWO NUMBERS";
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _apptitle,
+      home: Addtwo(),
+    );
+  }
+}
+class Addtwo extends StatefulWidget {
+  @override
+  _AddtwoState createState() => _AddtwoState();
+}
+
+class _AddtwoState extends State<Addtwo> {
+  TextEditingController num1 = new TextEditingController();
+  TextEditingController num2 = new TextEditingController();
+  String result = "0";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+        title: Text("ADD two numbers",style: TextStyle(color: Colors.black),),
+      ),
+       body:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Text("Enter 1 Number",style: TextStyle(fontSize: 15.0),),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 40.0,width: 70.0,
+                  color: Colors.blueAccent,
+                  child: TextField(
+                    controller: num1,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ),
+
+            ]
+          ),
+          SizedBox(height: 15.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Text("Enter 2 Number",style: TextStyle(fontSize: 15.0),),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                 height: 40.0,width: 70.0,
+                  color: Colors.blueAccent,
+                  child: TextField(
+                    controller: num2,
+                   decoration: InputDecoration(
+                     enabledBorder: OutlineInputBorder(
+                       borderSide: BorderSide(
+                         color: Colors.black,
+                       ),
+                     ),
+                   ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text("ADD"),
+            onPressed: (){
+              setState(() {
+                int sum = int.parse(num1.text) + int.parse(num2.text);
+                result  = sum.toString();
+              });
+            },
+          ),
+          Container(
+            child: Text(result, style: TextStyle(fontSize: 30.0),),
+            color: Colors.grey,
+            height: 40.0,width: 70.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
---
+++ fontsandcolors/android/app/src/main/kotlin/com/example/fontsandcolors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fontsandcolors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
